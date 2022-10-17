81283af5e07733e97a0aa9f7aced55e1a44b9255
Rafael Diego Gomes de Oliveira <rafael.gomes@serpro.gov.br>
2022-10-17 16:39:09 -0300

    fix: remove tag menu obsoleta e atualiza deps

---
+++ release.config.js
@@ -0,0 +1,13 @@
+const sharedConfig = require("@govbr-ds/release-config");
+
+module.exports = {
+  branches: [...sharedConfig.branches],
+  plugins: [
+    sharedConfig.plugins.commitAnalyzer,
+    sharedConfig.plugins.releaseNotes,
+    sharedConfig.plugins.changelog,
+    sharedConfig.plugins.gitlab,
+    sharedConfig.plugins.npm,
+    sharedConfig.plugins.git,
+  ],
+};
